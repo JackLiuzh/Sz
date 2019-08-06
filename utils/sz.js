cb2778fa6cb6bbd02fab6443489ead6b0fc69eeb
liu <2304450839@qq.com>
2019-08-06 17:33:53 +0800

    修改sz.js

--- utils/sz.js
+++ utils/sz.js
@@ -1,7 +1,7 @@
-// const URI = 'http://cs.szgk.cn/api.php'
+const URI = 'http://cs.szgk.cn/api.php'
 // const URI = 'http://192.168.1.219/api.php'
 //const URI = 'http://sz.com/api.php'
-const URI = 'https://www.szgk.cn/api.php'
+// const URI = 'https://www.szgk.cn/api.php'
 const fetch = require('./fetch')
 //微信登录
 function loginregister(params) {

c77590e3dd79d7a55f42f56864ca8da717731684
lmming <673219998@qq.com>
2019-08-06 17:05:10 +0800

    kecheng
    课程

---
+++ pages/course/course.js
@@ -0,0 +1,76 @@
+var app = getApp();
+Page({
+  data: {
+    winHeight: "",//窗口高度
+    currentTab: 0, //预设当前项的值
+    scrollLeft: 0, //tab标题的滚动条位置
+    main_id: '',
+    user_task_id: 0
+  },
+  // 滚动切换标签样式
+  switchTab: function (e) {
+    this.setData({
+      currentTab: e.detail.current
+    });
+    this.checkCor();
+  },
+  // 点击标题切换当前页时改变样式
+  swichNav: function (e) {
+    var cur = e.target.dataset.current;
+    if (this.data.currentTaB == cur) { return false; }
+    else {
+      this.setData({
+        currentTab: cur
+      })
+    }
+  },
+
+  //判断当前滚动超过一屏时，设置tab标题滚动条。
+  checkCor: function () {
+    if (this.data.currentTab > 4) {
+      this.setData({
+        scrollLeft: 300
+      })
+    } else {
+      this.setData({
+        scrollLeft: 0
+      })
+    }
+  },
+  onLoad: function (options) {
+    var that = this;
+    this.setData({
+      user_task_id: options.user_task_id,
+      main_id: options.main_id
+    });
+    that.initdata(options.id)
+    //  高度自适应
+    wx.getSystemInfo({
+      success: function (res) {
+        var clientHeight = res.windowHeight,
+          clientWidth = res.windowWidth,
+          rpxR = 750 / clientWidth;
+        var calc = clientHeight * rpxR - 130;
+        console.log(calc)
+        that.setData({
+          winHeight: calc
+        });
+      }
+    });
+  },
+  footerTap: app.footerTap,
+  //初始化数据
+  initdata: function (id) {
+    var that = this
+    var params = {
+      "id": 29334
+    }
+    app.sz.xcxVideoDetail(params).then(d => {
+      if (d.data.status == 0) {
+        that.setData({ list: d.data.data, video_url: d.data.video_url })
+      } else {
+        console.log("接口错误")
+      }
+    })
+  }
+})

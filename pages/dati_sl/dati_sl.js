5f0661cbe6d9347deafefdbfefb99a7a187ff306
xuhui-cc <1104636429@qq.com>
2019-09-24 15:54:16 +0800

    sl

--- pages/dati_sl/dati_sl.js
+++ pages/dati_sl/dati_sl.js
@@ -14,9 +14,12 @@ Page({
     imgs:[],  //添加图片
     // null_jiaojuan:true,
     current_cailiao: 0, //多材料滚动条预设当前项的值
-    // h_id:76,
+    h_id:85,
     sl_list:[],
     total:0,
+
+    img:[],
+
     
     // huadong: false,       //滑动查看下一题是否显示
   },
@@ -62,50 +65,106 @@ Page({
     }
   },
 
-//添加图片
-  chooseImg() {
-    let that = this;
-    // let len = this.data.imgs;
-    // if (len >= 9) {
-    //   this.setData({
-    //     lenMore: 1
-    //   })
-    //   return;
-    // }
-    wx.chooseImage({
-      success: (res) => {
-        let tempFilePaths = res.tempFilePaths;
-        let imgs = [];
-        for (let i = 0; i < tempFilePaths.length; i++) {
-          var token = wx.getStorageSync('token');       
-          wx.uploadFile({
-            url: 'http://cs.szgk.cn/api.php?',
-            filePath: tempFilePaths[i],
-            name: 'file',
-            formData: {
-              'file': tempFilePaths[i],
-              "token": token,
-              "action": "uploads", //action=uploads&authhash=445454554
-            },
-            success(r) {
-              let hhh = JSON.parse(r.data);
-              if (hhh.status == 1) {
-                imgs.unshift(hhh.data.src)
-                // that.data.img = 
-                that.setData({
-                  img: imgs
-                })
-                console.log(that.data.img)
-              } else {               
-                console.log('失败')
-                console.log(hhh.status)
-              }            
-            }
-          })          
+  chooseImg: function () {
+    var that = this,
+    img = that.data.img;
+    if (img.length < 2) {  //如果图片数量小于3张，可以直接获取图片
+      wx.chooseImage({
+        count: 1,     //默认9
+        sizeType: ['compressed'], //可以指定原图还是压缩图，默认二者都有
+        sourceType: ['album', 'camera'],//可以指定来源相册还是相机，默认二者都有
+        success: function (res) {
+          var tempFilesSize = res.tempFiles[0].size;  //获取图片的大小，单位B
+          // if (tempFilesSize <= 2000000) {   //图片小于或者等于2M时 可以执行获取图片
+            var tempFilePaths = res.tempFilePaths[0]; //获取图片
+            that.data.img.push(tempFilePaths);   //添加到数组
+            that.setData({
+              img: that.data.img
+            })
+          // } else {    //图片大于2M，弹出一个提示框
+          //   wx.showToast({
+          //     title: '上传图片不能大于2M!',  //标题
+          //     icon: 'none'       //图标 none不使用图标，详情看官方文档
+          //   })
+          // }
+          console.log(that.data.img)
         }
-      }
-    })
+      })
+    } else {  //大于三张时直接弹出一个提示框
+      wx.showToast({
+        title: '上传图片不能大于2张!',
+        icon: 'none'
+      })
+
+    }
+
   },
+
+  deleteImg: function(e) {
+    var index = e.currentTarget.dataset.index
+    console.log(index)
+    this.data.img.splice(index, 1)
+    console.log(this.data.img)
+    let csimg = this.data.img
+    this.setData({
+      img : csimg
+    })
+    // this.onShow()
+  },
+
+  submit_ans: function(){
+
+  },
+//添加图片
+  // chooseImg() {
+  //   let that = this;
+  //   // let len = this.data.imgs;
+  //   // if (len >= 9) {
+  //   //   this.setData({
+  //   //     lenMore: 1
+  //   //   })
+  //   //   return;
+  //   // }
+  //   wx.chooseImage({
+  //     success: (res) => {
+  //       let tempFilePaths = res.tempFilePaths;
+  //       let imgs = [];
+  //       imgs.push(tempFilePaths)
+  //       console.log(imgs)
+  //       this.setData({
+  //         img : imgs
+  //       })
+  //       console.log(this.data.img)
+  //       // for (let i = 0; i < tempFilePaths.length; i++) {
+  //       //   var token = wx.getStorageSync('token');       
+  //       //   wx.uploadFile({
+  //       //     url: 'http://cs.szgk.cn/api.php?',
+  //       //     filePath: tempFilePaths[i],
+  //       //     name: 'file',
+  //       //     formData: {
+  //       //       'file': tempFilePaths[i],
+  //       //       "token": token,
+  //       //       "action": "uploads", //action=uploads&authhash=445454554
+  //       //     },
+  //       //     success(r) {
+  //       //       let hhh = JSON.parse(r.data);
+  //       //       if (hhh.status == 1) {
+  //       //         imgs.unshift(hhh.data.src)
+  //       //         // that.data.img = 
+  //       //         that.setData({
+  //       //           img: imgs
+  //       //         })
+  //       //         console.log(that.data.img)
+  //       //       } else {               
+  //       //         console.log('失败')
+  //       //         console.log(hhh.status)
+  //       //       }            
+  //       //     }
+  //       //   })          
+  //       // }
+  //     }
+  //   })
+  // },
   /**
    * 生命周期函数--监听页面加载
    */
@@ -115,15 +174,18 @@ Page({
       huadong: huadong,
     })
     console.log(this.data.huadong)
-    var h_id = options.id
-    console.log(h_id)
-    let that = this
-    that.setData({
-      h_id: h_id,
-    })
-      var params = {
-        "h_id": h_id,
-      }
+    // var h_id = options.id
+    // console.log(h_id)
+    // let that = this
+    // that.setData({
+    //   h_id: h_id,
+    // })
+      // var params = {
+      //   "h_id": h_id,
+      // }
+    var params = {
+      "h_id": this.data.h_id,
+    }
       console.log(params)
       app.sz.xcxShenlunList(params).then(d => {
         if (d.data.status == 0) {
